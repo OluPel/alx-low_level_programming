4db9cd6158922672d79815793c88461e7704c72c
OluPel <olukayodepelumipeace@gmail.com>
2022-03-11 11:07:40 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - determines if the value of the random number
+ * * generated is positive, zero or negative
+ *
+ * Return: returns zero ar the end
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
