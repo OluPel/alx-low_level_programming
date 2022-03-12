4959d260543b6ac5968e8b394d6a97c7b92f468b
OluPel <olukayodepelumipeace@gmail.com>
2022-03-12 05:57:00 -0800

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,30 +6,24 @@
  */
 int main(void)
 {
-int number_left; /*Decarling statement*/
-int number_right;
+	int i, j;
 
-/*for 0-9 with ASCII*/
-for (number_left = 48; number_left <= 57; number_left++)
-{
-for (number_right = 48; number_right <= 57; number_right++)
-{
-putchar(number_left); /*print  number*/
-putchar (number_right);
-
-/*break program if the condition is true (if is 99)*/
-if ((number_left == 57) && (number_right == 57))
-{
-break; /*terminate the program*/
-}
-putchar(','); /*print (,)*/
-putchar (' '); /*print a space*/
-}
-
-}
-
-putchar('\n'); /*new line*/
-
-return (0);
-
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
